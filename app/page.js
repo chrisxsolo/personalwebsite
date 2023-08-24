bfced976ba76490b71620ce3daed25e684605ccb
Chris Solorzano <chrisxsolo177@gmail.com>
2023-08-23 21:31:28 -0700

    update

--- app/page.js
+++ app/page.js
@@ -6,6 +6,8 @@ import styles from './home.module.css';
 import NavBar from "./components/NavBar"
 import { useRef } from 'react'; // Import useRef
 import { useEffect } from 'react'; // Import useEffect
+export const dynamic = 'force-dynamic'
+
 
 
 
