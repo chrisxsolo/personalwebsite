d27d2af6d1c8c2a5bf5b917d1121c04b1667061d
Chris Solorzano <chrisxsolo177@gmail.com>
2023-08-24 12:25:59 -0700

    changed backend api

--- pages/api/get-recent-name.js
+++ pages/api/get-recent-name.js
@@ -2,22 +2,10 @@
 import prisma from '../../lib/prisma';
 
 export default async function handle(req, res) {
-  if (req.method === 'GET') {
-    try {
-      // Find the most recent message
-      const recentMessage = await prisma.contactMessage.findFirst({
+    const resentMessage = await prisma.contactMessage.findFirst({
         orderBy: {
-            id: 'desc'
+          id: 'desc',
         },
-      });
-
-      // Send the name from the most recent message
-      res.json({ name: recentMessage.name });
-    } catch (err) {
-      console.error(err);
-      res.status(500).json({ error: 'An error occurred while retrieving the name' });
+      })
+      res.json(resentMessage)
     }
-  } else {
-    res.status(405).json({ error: 'Method not allowed' });
-  }
-}
